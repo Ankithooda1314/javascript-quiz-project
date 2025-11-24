6d495b0ac63a044a0cbb20c9d95d1df657638586
Luis Castro <luiscastrosounds@gmail.com>
2025-11-24 17:45:02 +0000

    Updated alternative solution

--- src/question.js
+++ src/question.js
@@ -1,20 +1,20 @@
 class Question {
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    constructor(text, choices, answer, difficulty) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-        this.difficulty = difficulty;
-    }
-
-    shuffleChoices() {
-        // for(let i=this.choices.length -1; i > 0; i--) {
-
-        //     const j = Math.floor(Math.random() * (i+1))
-        //     [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]]
-        // }
-        this.choices.sort(() => Math.random() - 0.5);
-        return this.choices;
-    }
-
+  shuffleChoices() {
+    // //*************************** WORKING ALTERNATIVE WITHOUT .sort ************************
+    // for (let i = this.choices.length - 1; i > 0; i--) {
+    //   const j = Math.floor(Math.random() * (i + 1));
+    //   const temp = this.choices[i];
+    //   this.choices[i] = this.choices[j];
+    //   this.choices[j] = temp;
+    // }
+    this.choices.sort(() => Math.random() - 0.5);
+    return this.choices;
+  }
 }
