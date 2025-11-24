33fbdcd9cb5c9197d91a6e9932103c6b768b6b5a
Alexandra Domareski <aleexandra.junges@gmail.com>
2025-11-24 18:01:08 +0100

    implementing the classes Question and Quiz and started the methods

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,39 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex]
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions() {
+        // for(let i=this.questions.length -1; i > 0; i--) {
 
-    // 5. checkAnswer(answer)
+        //     const quiz = Math.floor(Math.random()*(i+1))
+        //     [this.questions[i], this.questions[quiz]] = [this.questions[quiz], this.questions[i]]
+        // }
+        this.questions.sort(() => Math.random() - 0.5);
+        return this.questions;
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer) {
+        const currentQuestion = this.getQuestion();
+        if(answer === currentQuestion.answer) {
+            this.correctAnswers++
+        }
+    }
+
+    hasEnded() {
+        return this.currentQuestionIndex >= this.questions.length
+    }
 }
